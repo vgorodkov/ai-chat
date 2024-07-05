38a320bfe0e4496e618f638dc81909db7f444cf6
Victor Gorodkov <gorodkov.victor@gmail.com>
2024-07-05 18:32:56 +0300

    refactor: use Typography component & decompose styles

---
+++ client/src/components/ChatListMessage/styles.ts
@@ -0,0 +1,11 @@
+import { spacings } from '@/constants/layout';
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  chatListMessage: {
+    borderRadius: 16,
+    paddingHorizontal: spacings.m,
+    paddingVertical: spacings.s,
+    borderWidth: 1,
+  },
+});
